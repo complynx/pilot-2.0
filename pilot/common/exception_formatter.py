3027284cc1dd2e3786d1dda3f300afea3993a042
Daniel Drizhuk <complynx@yandex.ru>
2017-01-16 14:17:15 +0300

    Module name failure (issue by Pavlo Sivirin)
    Exception fixed, but no real investigation yet done.

--- pilot/common/exception_formatter.py
+++ pilot/common/exception_formatter.py
@@ -16,18 +16,27 @@ def caught(exception, exc_info=sys.exc_info()):
     :param exc_info: Optional, the output of the `sys.exc_info()`, if it was caught earlier.
     """
 
-    tb = list(reversed(traceback.extract_tb(exc_info[2])))
+    try:
+        module_name = exc_info[0].__name__
+    except AttributeError:
+        module_name = '{none}'
+    try:
 
-    tb_str = "\n"
-    for i in tb:
-        tb_str += '{file}:{line} (in {module}): {call}\n'.format(file=i[0],
-                                                                 line=i[1],
-                                                                 module=i[2],
-                                                                 call=i[3])
-    # logger.debug("Traceback:" + tb_str + " ----")
-    thread = threading.currentThread()
-    msg = "%s: %s" % (exc_info[0].__name__, exception.message)
-    msg += "\nTraceback: (latest call first)" + tb_str + "Thread: %s(%d)" % (thread.getName(), thread.ident)
+        tb = list(reversed(traceback.extract_tb(exc_info[2])))
 
-    logger = logging.getLogger('Exception')
-    logger.handle(logger.makeRecord(logger.name, logging.ERROR, tb[0][0], tb[0][1], msg, (), None, tb[0][2]))
+        tb_str = "\n"
+        for i in tb:
+            tb_str += '{file}:{line} (in {module}): {call}\n'.format(file=i[0],
+                                                                     line=i[1],
+                                                                     module=i[2],
+                                                                     call=i[3])
+        # logger.debug("Traceback:" + tb_str + " ----")
+        thread = threading.currentThread()
+        msg = "%s: %s" % (module_name, exception.message)
+        msg += "\nTraceback: (latest call first)" + tb_str + "Thread: %s(%d)" % (thread.getName(), thread.ident)
+
+        logger = logging.getLogger('Exception')
+        logger.handle(logger.makeRecord(logger.name, logging.ERROR, tb[0][0], tb[0][1], msg, (), None, tb[0][2]))
+    except Exception as e:
+        logger = logging.getLogger('CRITICAL')
+        logger.handle(logger.makeRecord(logger.name, logging.CRITICAL, "caught()", 40, e.message, (), None, None))
